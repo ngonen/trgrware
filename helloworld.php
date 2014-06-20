a1c8dfd19e4d9fb07b86655e6475168ea41e05e0
Noam Gonen <me@noamgonen.com>
2014-06-20 04:02:46 -0700

    Commit 2
    dummy change

--- helloworld.php
+++ helloworld.php
@@ -32,7 +32,7 @@
 
 	    <form action="/sign" method="post">
 	      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-	      <div><input type="submit" value="Sign Guestbook"></div>
+	      <div><input type="submit" value="Go Home"></div>
 	    </form>
 
 		<?php } ?>
