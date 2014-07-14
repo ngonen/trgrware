a1590a5c3b550c7edba4e1728fa2f3156d921b04
Anton <Anton.Dubynin@globallogic.com>
2014-07-14 16:08:50 +0300

    Twitter trigger update request changed.

--- webapp/js/trigger.js
+++ webapp/js/trigger.js
@@ -113,7 +113,7 @@ function TwitterTrigger(properties) {
         return {
             assetId: _self.asset_id,
             eventType: _self.type,
-            retriggerPeriod: properties.rwp,
+            center: asset.lat + "|" + asset.lng,
             hashTag: properties.hashtag,
             userName: properties.username,
             campaigns: JSON.stringify(campaigns)
