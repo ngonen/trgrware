2e73152a745d8d1aaaa2c3468124979c4f84621e
ihor <my@email.com>
2014-07-15 11:52:07 +0300

    CR (formatting).

--- webapp/protected/modules/demo/controllers/DefaultController.php
+++ webapp/protected/modules/demo/controllers/DefaultController.php
@@ -97,8 +97,8 @@ class DefaultController extends IDemoBaseController
 
         $resultXML = $this->getWeatherInRadius($center, $radius);
         $status = $resultXML->Weather->Conditions && count($resultXML->Weather->Conditions->Station) > 0;
+        $response["refreshKey"] = $refreshKey;
 
-		$response["refreshKey"] = $refreshKey;
         if ($status) {
             $response["status"] = $status;
             $response['message'] = "Weather stations were successfully returned.";
