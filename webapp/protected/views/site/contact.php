fd06ef55e5eb72279adb54fc454cedb58e90da0f
ihor <my@email.com>
2014-07-17 16:54:37 +0300

    Contact Us, About us pages. Added iframe on Home page. Design updates.

--- webapp/protected/views/site/contact.php
+++ webapp/protected/views/site/contact.php
@@ -25,11 +25,11 @@ If you have business inquiries or other questions, please fill out the following
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
+<?php $form = $this->beginWidget('CActiveForm', array(
 	'id'=>'contact-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
+	'enableClientValidation' => true,
+	'clientOptions' => array(
+		'validateOnSubmit' => true,
 	),
 )); ?>
 
@@ -49,11 +49,11 @@ If you have business inquiries or other questions, please fill out the following
 		<?php echo $form->error($model,'email'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'subject'); ?>
-		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'subject'); ?>
-	</div>
+<!--	<div class="row">-->
+<!--		--><?php //echo $form->labelEx($model,'subject'); ?>
+<!--		--><?php //echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+<!--		--><?php //echo $form->error($model,'subject'); ?>
+<!--	</div>-->
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'body'); ?>
@@ -61,18 +61,18 @@ If you have business inquiries or other questions, please fill out the following
 		<?php echo $form->error($model,'body'); ?>
 	</div>
 
-	<?php if(CCaptcha::checkRequirements()): ?>
-	<div class="row">
-		<?php echo $form->labelEx($model,'verifyCode'); ?>
-		<div>
-		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
-		</div>
-		<div class="hint">Please enter the letters as they are shown in the image above.
-		<br/>Letters are not case-sensitive.</div>
-		<?php echo $form->error($model,'verifyCode'); ?>
-	</div>
-	<?php endif; ?>
+<!--	--><?php //if(CCaptcha::checkRequirements()): ?>
+<!--	<div class="row">-->
+<!--		--><?php //echo $form->labelEx($model,'verifyCode'); ?>
+<!--		<div>-->
+<!--		--><?php //$this->widget('CCaptcha'); ?>
+<!--		--><?php //echo $form->textField($model,'verifyCode'); ?>
+<!--		</div>-->
+<!--		<div class="hint">Please enter the letters as they are shown in the image above.-->
+<!--		<br/>Letters are not case-sensitive.</div>-->
+<!--		--><?php //echo $form->error($model,'verifyCode'); ?>
+<!--	</div>-->
+<!--	--><?php //endif; ?>
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Submit'); ?>
